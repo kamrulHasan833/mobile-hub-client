b1a89d36b12c65fa3b7bfa88b021b50c2c1a716a
Kamrul Hasan <kamrul3496hasan@gmail.com>
2023-12-20 14:54:14 +0600

    initial set-up added

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import router from "./routes/router";
+const client = new QueryClient();
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <QueryClientProvider client={client}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
